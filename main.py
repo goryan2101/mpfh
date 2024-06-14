748ca9e11dae20e9278fc3639bff9e87254064da
goryan2101 <153424343+goryan2101@users.noreply.github.com>
2024-06-14 10:55:57 +0300

    Upload program files

---
+++ main.py
@@ -0,0 +1,66 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.slider import Slider
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.clock import Clock
+from kivy.config import Config
+import subprocess
+
+Config.set("graphics", "width", 640)
+Config.set("graphics", "height", 480)
+
+main_layout = BoxLayout(orientation="vertical")
+volume_layout = BoxLayout(orientation="vertical")
+volume = Slider(min=0, max=100, value=int(subprocess.run(["pamixer", "--get-volume"], capture_output=True).stdout))
+volume_label = Label(text=str(int(volume.value)), font_size=25)
+bright_layout = BoxLayout(orientation="vertical")
+bright = Slider(min=1, max=255, value=int(subprocess.run(["brightnessctl", "get"], capture_output=True).stdout))
+bright_label = Label(text=str(int(bright.value)), font_size=25)
+timedate = Label(text=subprocess.run(["date"], capture_output=True).stdout.decode("utf-8"), font_size=18)
+battery = Label(text=
+                "Baterry:" +
+                subprocess.run(["cat", "/sys/class/power_supply/BAT0/capacity"], capture_output=True).stdout.decode("utf-8") +
+                subprocess.run(["cat", "/sys/class/power_supply/BAT0/capacity_level"], capture_output=True).stdout.decode("utf-8"),
+                font_size=18, size=[640, 18])
+config_button = Button(text="Open config file", font_size=16)
+
+
+def open_config_file(instance):
+    subprocess.run(["foot", "nano", "~/.config/hypr/hyprland.conf"])
+
+config_button.bind(on_press=open_config_file)
+
+def update_volume(dt):
+    subprocess.run(["pamixer", "--set-volume", str(int(volume.value))])
+    volume_label.text = "Volume: " + str(int(volume.value))
+
+def update_brightness(dt):
+    subprocess.run(["brightnessctl", "set", str(int(bright.value))])
+    bright_label.text = "Brightness: " + str(int(bright.value))
+
+def update_timedate(dt):
+    timedate.text = subprocess.run(["date"], capture_output=True).stdout.decode("utf-8")
+
+def update_battery(dt):
+    battery.text = "Baterry:" + subprocess.run(["cat", "/sys/class/power_supply/BAT0/capacity"], capture_output=True).stdout.decode("utf-8") + subprocess.run(["cat", "/sys/class/power_supply/BAT0/capacity_level"], capture_output=True).stdout.decode("utf-8")
+
+class SettingsApp(App):
+    def build(self):
+        volume_layout.add_widget(volume_label)
+        volume_layout.add_widget(volume)
+        main_layout.add_widget(volume_layout)
+        bright_layout.add_widget(bright_label)
+        bright_layout.add_widget(bright)
+        main_layout.add_widget(bright_layout)
+        main_layout.add_widget(timedate)
+        main_layout.add_widget(battery)
+        main_layout.add_widget(config_button)
+        return main_layout
+        
+if __name__ == "__main__":
+    Clock.schedule_interval(update_volume, 1/20)
+    Clock.schedule_interval(update_brightness, 1/20)
+    Clock.schedule_interval(update_timedate, 1)
+    Clock.schedule_interval(update_battery, 1/20)
+    SettingsApp().run()
